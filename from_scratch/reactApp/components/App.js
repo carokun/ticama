a7279f1744cce3232af7fd818d86d867d1b628e8
caroline <carokun@sas.upenn.edu>
2017-07-09 17:25:21 -0700

    7/9/5:30pm

--- from_scratch/reactApp/components/App.js
+++ from_scratch/reactApp/components/App.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Home';
 import UserProfile from './UserProfile';
 import NavBar from './NavBar';
 
+//import provider to pass down store
+import { Provider } from 'react-redux';
+
+//implementing redux
+import { createStore } from 'redux';
+
+//import the reducer
+import counters from '../reducers/index.js';
+
+//link store to a reducer
+const store = createStore(counters);
+
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route path='/' component={NavBar} />
-          <Route exact path='/' component={Home} />
-          <Route exact path='/userProfile' component={UserProfile} />
-        </div>
-      </BrowserRouter>
+      <Provider store = {store}>
+        <BrowserRouter>
+          <div>
+            {this.props.value.counter1}
+            {this.props.value.counter2}
+            <button onClick={this.props.onIncrement}>+</button>
+            <Route path='/' component={NavBar} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/userProfile' component={UserProfile} />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
 
-ReactDOM.render(
-  <App/>,
+const render = () => {ReactDOM.render(
+  <App value={store.getState()} onIncrement={() => store.dispatch({type: 'INCREMENT'})}/>,
   document.getElementById('root')
-)
+)}
+
+//render function will run anytime the state changes because we subscribe
+//it as listener
+store.subscribe(render);
+
+//call render to set initial state
+render();
