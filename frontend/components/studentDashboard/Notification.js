6e27a6a5a9cc9ee01054f58a3dea245aeeb2ea61
Tiffany <tifchang@wharton.upenn.edu>
2017-08-04 16:23:20 -0700

    yo

--- frontend/components/studentDashboard/Notification.js
+++ frontend/components/studentDashboard/Notification.js
@@ -4,13 +4,17 @@ import { connect } from 'react-redux';
 class Notification extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isOrganizer: this.props.isOrganizer,
+      sender: this.props.sender
+    }
   }
 
   render() {
     return (
-    <div className="notification is-danger">
+    <div className={this.state.isOrganizer ? "notification is-danger" : "notification is-info"}>
       <button className="delete"></button>
-      <strong>McKinsey & Co.</strong><br/>
+      <strong>{this.props.sender}</strong><br/>
       Attention participants! We are releasing edits to slide 40 in the packet. Please checkout the new updates.
     </div>
 
