cd2c290fff5ff3ae0ba50274c5fd7a12103d8a61
caroline <carokun@sas.upenn.edu>
2017-08-04 14:46:17 -0700

    fkldjlkf

--- frontend/components/studentProfile/BasicStudent.js
+++ frontend/components/studentProfile/BasicStudent.js
@@ -9,99 +9,98 @@ class BasicStudent extends Component {
   render() {
     return(
       <div className="tile is-parent is-vertical is-3">
-        <figure className="image is-4by3">
-          <img src="http://bulma.io/images/placeholders/640x480.png" />
-        </figure>
+        <article>
+          <figure className="image is-4by3">
+            <img src="http://bulma.io/images/placeholders/640x480.png" />
+          </figure>
+        </article>
         <article className="tile is-parent is-vertical notification" style={{padding: '30px', backgroundColor: 'white', boxShadow: '0 0 0.5px 0 #dbdbdb'}}>
-            <div className='tile is-child'>
+            <article className='tile media is-child'>
               <p className="title is-4">Matt Pearing</p>
               <p className="subtitle is-6">Data Scientist</p>
-              <p>this is a small f;kldsijflkjfl;ksdjf;lksjfblurb this is a small blurb this is a small blurb this is a small blurb this is a small blurb</p>
-            </div>
-          <hr/>
-        <div className='tile is-parent'>
-          <div className='tile is-child is-2'>
-            <span className="icon">
-              <i className="fa fa-graduation-cap"></i>
-            </span>
-          </div>
-          <div className='tile is-child is-10'>
-            <div>
-              University of Pennsylvania
-            </div>
-            <div>
-              Computer Science and Economics
-            </div>
-            <div>
-              2020
-            </div>
-          </div>
-        </div>
-        <hr/>
-        <div className='tile is-parent'>
-          <div className='tile is-child is-2'>
-            <span className="icon">
-              <i className="fa fa-lightbulb-o"></i>
-            </span>
-          </div>
-          <div className='tile is-child is-10'>
-            <div className='tags'>
-              <span className="tag is-primary is-medium">
-                interest 1
-              </span>
-              <span className="tag is-primary is-medium">
-                interest 2
-              </span>
-              <span className="tag is-primary is-medium">
-                interest 3
-              </span>
-              <span className="tag is-primary is-medium">
-                interest 4
-              </span>
-              <span className="tag is-primary is-medium">
-                interest 5
-              </span>
-              <span className="tag is-primary is-medium">
-                interest 6
-              </span>
-            </div>
-          </div>
-        </div>
-        <hr/>
-
-        <div className='tile is-parent'>
-          <div className='tile is-child is-2'>
-            <span className="icon">
-              <i className="fa fa-link"></i>
-            </span>
-          </div>
-          <div className='tile is-child is-10'>
-            www.carokun.github.io
-          </div>
-        </div>
-        <div className='tile is-parent'>
-          <div className='tile is-child is-2'>
-            <span className="icon">
-              <i className="fa fa-linkedin"></i>
-            </span>
-          </div>
-          <div className='tile is-child is-10'>
-            www.linkedin.com/carolineokun
-          </div>
-        </div>
-        <hr/>
-        <div className='tile is-parent'>
-          <div className='tile is-child is-2'>
-            <span className="icon">
-              <i className="fa fa-paper-plane"></i>
-            </span>
-          </div>
-          <div className='tile is-child is-10'>
-            <a style={{textDecoration: 'none'}}>
-              Resume
-            </a>
-          </div>
-        </div>
+              <p>this is a small jf;lksjfblurb this is a small blurb this is a small blurb this is a small blurb this is a small blurb</p>
+            </article>
+            <article className='tile is-parent media'>
+              <div className='tile is-child is-2'>
+                <span className="icon">
+                  <i className="fa fa-graduation-cap"></i>
+                </span>
+              </div>
+              <div className='tile is-child is-10'>
+                <div>
+                  University of Pennsylvania
+                </div>
+                <div>
+                  Computer Science and Economics
+                </div>
+                <div>
+                  2020
+                </div>
+              </div>
+            </article>
+            <article className='tile is-parent media'>
+              <div className='tile is-child is-2'>
+                <span className="icon">
+                  <i className="fa fa-lightbulb-o"></i>
+                </span>
+              </div>
+              <div className='tile is-child is-10'>
+                <div className='tags'>
+                  <span className="tag is-primary is-medium">
+                    interest 1
+                  </span>
+                  <span className="tag is-primary is-medium">
+                    interest 2
+                  </span>
+                  <span className="tag is-primary is-medium">
+                    interest 3
+                  </span>
+                  <span className="tag is-primary is-medium">
+                    interest 4
+                  </span>
+                  <span className="tag is-primary is-medium">
+                    interest 5
+                  </span>
+                  <span className="tag is-primary is-medium">
+                    interest 6
+                  </span>
+                </div>
+              </div>
+            </article>
+            <article className='media tile is-vertical'>
+              <div className='tile is-parent'>
+                <div className='tile is-child is-2'>
+                  <span className="icon">
+                    <i className="fa fa-linkedin"></i>
+                  </span>
+                </div>
+                <div className='tile is-child is-10'>
+                  www.linkedin.com/carolineokun
+                </div>
+              </div>
+              <div className='tile is-parent'>
+                <div className='tile is-child is-2'>
+                  <span className="icon">
+                    <i className="fa fa-link"></i>
+                  </span>
+                </div>
+                <div className='tile is-child is-10'>
+                  www.carokun.github.io
+                </div>
+              </div>
+            </article>
+            <article className='tile is-parent media'>
+              <div className='tile is-child is-2'>
+                <span className="icon">
+                  <i className="fa fa-paper-plane"></i>
+                </span>
+              </div>
+              <div className='tile is-child is-10'>
+                <a style={{textDecoration: 'none'}}>
+                  Resume
+                </a>
+              </div>
+            </article>
         </article>
       </div>
     )
--- frontend/components/studentProfile/CompetitionExperience.js
+++ frontend/components/studentProfile/CompetitionExperience.js
@@ -7,54 +7,84 @@ class CompetitionExperience extends Component {
   render() {
     return(
       <article className="tile is-child notification is-vertical">
-        <div>
+        <article className='media'>
           <p className="title">Competition Experience</p>
-        </div>
-        <hr/>
-        <table className='table is-fullwidth'>
-          <tbody>
-            <tr>
-              <td>
-                <div>
-                  Deloitte Undergraduate Case Competition
-                </div>
-                <div>
-                  2017
-                </div>
-                <div>
-                  Philadelphia, PA
-                </div>
+        </article>
+        <article className='media'>
+          <table className='table is-fullwidth'>
+            <tbody>
+              <tr>
+                <td className='level'>
+                  <div className='level-left tile is-vertical'>
+                    <div>
+                      Deloitte Undergraduate Case Competition
+                    </div>
+                    <div>
+                      2017
+                    </div>
+                    <div>
+                      Philadelphia, PA
+                    </div>
+                  </div>
+                  <div className='level-right' style={{marginTop: '30px'}}>
+                    <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+                    <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+                    <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className='level'>
+                  <div className='level-left tile is-vertical'>
+                    <div>
+                      Deloitte Undergraduate Case Competition
+                    </div>
+                    <div>
+                      2017
+                    </div>
+                    <div>
+                      Philadelphia, PA
+                    </div>
+                  </div>
+                  <div className='level-right' style={{marginTop: '30px'}}>
+                    <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+                    <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+                    <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+                    <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className='level'>
+                  <div className='level-left tile is-vertical'>
+                    <div>
+                      Deloitte Undergraduate Case Competition
+                    </div>
+                    <div>
+                      2017
+                    </div>
+                    <div>
+                      Philadelphia, PA
+                    </div>
+                  </div>
+                  <div className='level-right tile is-vertical level'>
+                    <div className='tile level-right'>
+                      <div className='tag is-warning'>
+                        1st
+                      </div>
+                    </div>
+                    <span style={{height: '15px'}}></span>
+                    <div className='level-right tile'>
+                      <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+                      <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </article>
 
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>
-                  Deloitte Undergraduate Case Competition
-                </div>
-                <div>
-                  2017
-                </div>
-                <div>
-                  Philadelphia, PA
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>
-                  Deloitte Undergraduate Case Competition
-                </div>
-                <div>
-                  2017
-                </div>
-                <div>
-                  Philadelphia, PA
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </article>
     )
   }
