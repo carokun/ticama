be2c4c10b7c34bb0ff303d4d7507e297af797af8
caroline <carokun@sas.upenn.edu>
2017-08-04 13:48:22 -0700

    jkgjhk

---
+++ frontend/components/studentCompetition/CompetitionOverview.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+
+class CompetitionOverview extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return(
+      <div>
+        <article className="tile is-parent is-vertical notification" style={{padding: '30px', backgroundColor: 'white', boxShadow: '0 0 0.5px 0 #dbdbdb'}}>
+          <div className='tile is-parent is-vertical'>
+            <div className='tile is-child'>
+              <p style={{fontSize:'23px'}}>Undergraduate Case Competition</p>
+              <p style={{fontSize:'18px'}}>McKinsey & Co</p>
+            </div>
+          </div>
+        <div className='tile is-parent'>
+          <div className='tile is-child is-2'>
+            <span className="icon">
+              <i className="fa fa-calendar"></i>
+            </span>
+          </div>
+          <div className='tile is-child is-10'>
+            <div>
+              5/11/2017 - 5/13/2017
+            </div>
+          </div>
+        </div>
+        <div className='tile is-parent'>
+          <div className='tile is-child is-2'>
+            <span className="icon">
+              <i className="fa fa-location-arrow"></i>
+            </span>
+          </div>
+          <div className='tile is-child is-10'>
+            <div>
+              401 Walnut St.
+            </div>
+            <div>
+              Philadelphia, PA 19104
+            </div>
+
+          </div>
+          <div className='tile is-child'>
+            <hr/>
+          </div>
+        </div>
+
+
+        <div className='tile is-parent'>
+          <div className='tile is-child'>
+            <div>
+              This is a brief description blah blaaah blah blaaah blah blaaah blah blaaah blah blaaah blah blaaah blah blaaah blah blaaah blah blaaah blah blaaah blah blaaah blah blaaah blah blaaah blah blaaaah blaaah
+            </div>
+          </div>
+        </div>
+        <div style={{height: '10px'}}>
+        </div>
+        <div className='tile is-parent is-vertical'>
+          <div className='tile is-child'>
+            <div style={{padding: '0px', fontSize: '20px'}}>
+              Members
+              <hr />
+            </div>
+          </div>
+
+          <div className='tile is-child'>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+            <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
+          </div>
+        </div>
+        <div className='tile is-parent'>
+          <div className='tile is-child'>
+            <button className='button is-success'>
+              Team Portal
+            </button>
+          </div>
+        </div>
+        </article>
+      </div>
+    )
+  }
+}
+
+export default CompetitionOverview;
--- frontend/views/StudentCompetition.js
+++ frontend/views/StudentCompetition.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
-import { loginUser } from '../actions/AuthActions'
+import CompetitionOverview from '../components/studentCompetition/CompetitionOverview.js'
+import MessageBoard from '../components/studentCompetition/MessageBoard.js'
 
 class StudentCompetition extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      username: '',
-      password: ''
-    }
   }
 
   render() {
     return (
       <div>
-        hi :)
+        <nav className="breadcrumb has-bullet-separator" aria-label="breadcrumbs" style={{paddingTop: '10px'}}>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li className="is-active"><a href="#" aria-current="page">McKinsey & Co. Undergraduate Case Competition</a></li>
+          </ul>
+        </nav>
+        <div className='tile is-ancestor' style={{padding: '0px 20px', margin: '0px'}}>
+          <div className='tile is-3 is-parent is-vertical'>
+            <CompetitionOverview />
+          </div>
+          <div className='tile is-9 is-parent is-vertical'>
+            <MessageBoard />
+          </div>
+        </div>
       </div>
+
     )
   }
 }
@@ -30,7 +40,6 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    loginUser: (username, password) => dispatch(loginUser(dispatch, username, password))
   }
 };
 
