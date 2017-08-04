6bcf507ff579e05f46ba38ac873b33bc6b044747
Tiffany <tifchang@wharton.upenn.edu>
2017-08-04 12:16:11 -0700

    yo

--- frontend/components/studentDashboard/CurrentCompetition.js
+++ frontend/components/studentDashboard/CurrentCompetition.js
@@ -8,16 +8,41 @@ class CurrentCompetition extends Component {
 
   render() {
     return (
-    <div className="tile is-parent is-success notification is-success">
-      <div className="tile is-child is-2">
-        PIC
+
+
+      <div className="box is-card">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-32x32">
+              <img src="http://bulma.io/images/placeholders/128x128.png" alt="Image" />
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>McKinsey & Co.</strong>
+                <br />
+                <nav className="level is-mobile">
+                  <div className="level-left">
+                    <div className="level-item">
+                      <span className="icon">
+                        <i className="fa fa-clock-o"></i>
+                      </span>
+                      <span style={{color: 'red'}}>  4h 5m</span>
+                    </div>
+                  </div>
+                  <div className="level-right">
+                    <div className="level-item">
+                      <button className="button is-info">Open</button>
+                    </div>
+                  </div>
+                </nav>
+              </p>
+            </div>
+          </div>
+        </article>
       </div>
-      <div className="tile is-child is-10">
-        <p><strong> McKinsey Case Competition </strong></p>
-        <p> Time until submission: </p>
-        <button className='button is-warning'>Portal</button>
-      </div>
-    </div>
+
     )
   }
 }
