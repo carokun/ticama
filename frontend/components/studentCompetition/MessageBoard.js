e2645a3f63bcbf2f70ef6e928405dfdf6b019188
Tiffany <tifchang@wharton.upenn.edu>
2017-08-14 15:05:46 -0700

    this is done woop woop! letts goooooo

--- frontend/components/studentCompetition/MessageBoard.js
+++ frontend/components/studentCompetition/MessageBoard.js
@@ -10,7 +10,13 @@ class MessageBoard extends Component {
       password: ''
     }
   }
-
+  noPosts() {
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+      <h2 className="title is-1" style={{textAlign: 'center'}}>There are no posts right now!</h2>
+      <p className="title is-1" style={{textAlign: 'center', fontSize: '60px'}}>🤷</p>
+      <h2 className="title is-1" style={{textAlign: 'center'}}>Make one up there!</h2>
+    </div>
+  }
   render() {
     return (
       <div className='notification tile is-child' style={{backgroundColor: 'transparent'}}>
