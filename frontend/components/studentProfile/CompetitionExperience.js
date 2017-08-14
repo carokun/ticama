0b9b02416749d2c9d54da88e1a115e43d65094cb
caroline <carokun@sas.upenn.edu>
2017-08-14 12:27:43 -0700

    dkjsdklf

--- frontend/components/studentProfile/CompetitionExperience.js
+++ frontend/components/studentProfile/CompetitionExperience.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class CompetitionExperience extends Component {
   constructor(props) {
@@ -6,18 +7,20 @@ class CompetitionExperience extends Component {
   }
 
   renderCompetitionExperience(item) {
+    console.log(item);
     return (
-      <tr key={item}>
+      <tr key={item._id}>
+        <Link to={'/competition/' + item._id}>
         <td className='level'>
           <div className='level-left tile is-vertical'>
             <div>
-              Deloitte Undergraduate Case Competition
+              {item.title}
             </div>
             <div>
-              2017
+              {item.startDate} - {item.endDate}
             </div>
             <div>
-              Philadelphia, PA
+              {item.location}
             </div>
           </div>
           <div className='level-right' style={{marginTop: '30px'}}>
@@ -26,11 +29,13 @@ class CompetitionExperience extends Component {
             <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
           </div>
         </td>
+        </Link>
       </tr>
     )
   }
 
   render() {
+    console.log(this.props.competitionExperience);
     return(
       <article className="tile is-child notification is-vertical">
         <article className='media'>
@@ -39,35 +44,7 @@ class CompetitionExperience extends Component {
         <article className='media'>
           <table className='table is-fullwidth'>
             <tbody>
-              {[1, 2, 3].map((item) => this.renderCompetitionExperience(item))}
-{/*
-              <tr>
-                <td className='level'>
-                  <div className='level-left tile is-vertical'>
-                    <div>
-                      Deloitte Undergraduate Case Competition
-                    </div>
-                    <div>
-                      2017
-                    </div>
-                    <div>
-                      Philadelphia, PA
-                    </div>
-                  </div>
-                  <div className='level-right tile is-vertical level'>
-                    <div className='tile level-right'>
-                      <div className='tag is-warning'>
-                        1st
-                      </div>
-                    </div>
-                    <span style={{height: '15px'}}></span>
-                    <div className='level-right tile'>
-                      <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
-                      <img src="http://bulma.io/images/placeholders/32x32.png" style={{margin: '0px 5px', borderRadius: '100px'}}/>
-                    </div>
-                  </div>
-                </td>
-              </tr> */}
+              {this.props.competitionExperience.map((item) => this.renderCompetitionExperience(item))}
             </tbody>
           </table>
         </article>
