8a55fe3ff66f4fb9232a35c811a3ebfe156073dc
Tiffany <tifchang@wharton.upenn.edu>
2017-08-14 16:28:13 -0700

    this is done woop woop! letts goooooo

--- frontend/views/StudentCompetition.js
+++ frontend/views/StudentCompetition.js
@@ -5,6 +5,8 @@ import CompetitionOverview from '../components/studentCompetition/CompetitionOve
 import MessageBoard from '../components/studentCompetition/MessageBoard.js'
 import axios from 'axios';
 import PostModal from '../components/studentCompetition/PostModal';
+import RegisterModal from '../components/studentCompetition/RegisterModal';
+
 
 class StudentCompetition extends Component {
   constructor(props) {
@@ -12,8 +14,11 @@ class StudentCompetition extends Component {
     this.state = {
       competition: '',
       modal: false,
+      register: false
     }
     this.closeModal = this.closeModal.bind(this);
+    this.startRegister = this.startRegister.bind(this);
+    this.endRegister = this.endRegister.bind(this);
   }
   componentDidMount() {
     const path = this.props.history.location.pathname
@@ -29,11 +34,18 @@ class StudentCompetition extends Component {
   closeModal() {
     this.setState({modal: false})
   }
+  startRegister() {
+    this.setState({register: true})
+  }
+  endRegister() {
+    this.setState({register: false})
+  }
   render() {
     if (this.state.competition) {
       return (
         <div className="container is-fluid">
           { this.state.modal && <PostModal closeModal={this.closeModal}/> }
+          { this.state.register && <RegisterModal endRegister={this.endRegister}/> }
           <div className="top-spacer-big">
             <button className="button is-info is-large" style={{position: 'absolute', top: '20px', right: '78px', zIndex: '5'}} onClick={() => this.setState({modal: true})}>
               <span className="icon">
@@ -44,7 +56,7 @@ class StudentCompetition extends Component {
           </div>
           <div className='tile is-ancestor' style={{padding: '0px 20px', margin: '0px'}}>
             <div className='tile is-3 is-parent is-vertical'>
-              <CompetitionOverview comp={this.state.competition}/>
+              <CompetitionOverview startRegister={this.startRegister} comp={this.state.competition}/>
             </div>
             <div className='tile is-9 is-parent is-vertical'>
               <MessageBoard comp={this.state.competition}/>
